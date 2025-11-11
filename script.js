9348f738ef92c30c2bb49b505d2155add17bb3f8
Lechintan Joshua <jlechintan79@gmail.com>
2025-11-11 20:59:41 +0200

    Added functionalities to js Getters for book atributes Functions to create Books and HTML card

--- script.js
+++ script.js
@@ -1,11 +1,116 @@
 const dialog = document.querySelector('dialog');
 const addButton = document.querySelector('.add-book');
 const submitBtn = document.querySelector('.submit');
+const cancelBtn = document.querySelector('.cancel');
+
+const myLibrary = [];
+
+function Book (name, author, pages, status) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.id = crypto.randomUUID();
+}
+
+Book.prototype.getName = function () {
+    return this.name;
+}
+
+Book.prototype.getAuthor = function () {
+    return this.author;
+}
+
+Book.prototype.getPages = function () {
+    return this.pages;
+}
+
+Book.prototype.getStatus = function () {
+    return this.status;
+}
+
+Book.prototype.getID = function () {
+    return this.id;
+}
+
+Book.prototype.changeStatus = function () {
+    if (this.status === 'Reading...') {
+        this.status = 'Finished';
+    } else {
+        this.status = 'Reading...'
+    }
+}
+
+function addBookToLibrary(Book) {
+    myLibrary.push(Book);
+}
+
+function getBookFromForm () {
+    const inputName = document.querySelector('#name').value;
+    const inputAuthor = document.querySelector('#author').value;
+    const inputPages = document.querySelector('#pages').value;
+    const status = document.querySelector('input[name="status"]:checked').value;
+    
+    return new Book(inputName, inputAuthor, inputPages, status);
+}
+
+function createBookHTMLElement (Book) {
+    const card = document.createElement('div');
+    const title = document.createElement('h3');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const status = document.createElement('p');
+    const buttons = document.createElement('div');
+    const delBtn = document.createElement('button');
+    const changeStat = document.createElement('button');
+
+    title.textContent = Book.getName();
+    author.textContent = Book.getAuthor();
+    pages.textContent = Book.getPages();
+    status.textContent = Book.getStatus();
+    buttons.className = 'buttons';
+    delBtn.className = 'del-book';
+    changeStat.className = 'change-status-book';
+
+    buttons.appendChild(delBtn);
+    buttons.appendChild(changeStat);
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(status);
+    card.appendChild(buttons);
+
+    return card;
+}
 
 addButton.addEventListener('click', () => {
     dialog.showModal();
 });
 
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener('click', (event) => {
+    let book = getBookFromForm();
+    addBookToLibrary(book);
     dialog.close();
 });
+
+cancelBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    dialog.close();
+});
+
+
+//             <div class="card">
+//     <div class="header">
+//         <h3>Title</h3>
+//     </div>
+
+//     <p class="book-name">Name</p>
+//     <p class="book-author">Author</p>
+//     <p class="book-pages">Pages</p>
+//     <p class="book-status">Status</p>
+
+//     <div class="buttons">
+//         <button class="change-status book">Change status</button>
+//         <button class="del book">Delete</button>
+//     </div>
+// </div>
